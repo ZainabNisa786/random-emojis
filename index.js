34ef2752c2d8e446f0c2afe144424197d5b7db82
Zainab Nisa <nisazainab53@gmail.com>
2023-02-22 21:20:03 +0500

    random emojis generator

---
+++ index.js
@@ -0,0 +1,11 @@
+let fighters = ["🐉", "🐥", "🐊","🦉", "🦍", "🐢", "🐩", "🐰", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+
+    let randomIndexOne = Math.floor( Math.random() * fighters.length )
+    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+    stageEl.textContent = fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
+})
